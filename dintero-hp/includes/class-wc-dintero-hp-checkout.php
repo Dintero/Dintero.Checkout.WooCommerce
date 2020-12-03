a3e2f256cae47b86d747bc54c1b8a7ed6df99b9f
Anders Emil Salvesen <andersems@gmail.com>
2020-12-03 15:14:43 +0100

    Replace underscore with dash

--- dintero-hp/includes/class-wc-dintero-hp-checkout.php
+++ dintero-hp/includes/class-wc-dintero-hp-checkout.php
@@ -1186,7 +1186,7 @@ class WC_Dintero_HP_Checkout extends WC_Checkout {
 					        .embed({
 					            container,
 					            sid: \"" . esc_attr( $id ) . "\",
-					            language: \"" . get_user_locale() . "\",
+					            language: \"" . str_replace("_", "-", get_user_locale()) . "\",
 					            onPaymentAuthorized : function(event,checkout){
 					            	 
 					            	 	jQuery('.loader').css('display','block');
@@ -1363,7 +1363,7 @@ class WC_Dintero_HP_Checkout extends WC_Checkout {
 						        .embed({
 						            container,
 						            sid: \"" . esc_attr( $id ) . "\",
-						            language: \"" . get_user_locale() . "\",
+						            language: \"" . str_replace("_", "-", get_user_locale()) . "\",
 						            onSession: function(event, checkout) {
 						                console.log(\"session\", event.session);
 						                var ss = event.session;
