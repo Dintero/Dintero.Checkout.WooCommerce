b6e1f2949acadf488de35c540704ecc8c6fb44a3
Anders Emil Salvesen <anders@dintero.com>
2021-03-10 11:42:17 +0100

    Release 2021.03.10 (#33)
    * Setting to allow orders without shipping
    * Error messages when API calls fail

--- dintero-hp.php
+++ dintero-hp.php
@@ -3,7 +3,7 @@
 Plugin Name: Dintero Checkout
 Description: Dintero Checkout - Express Checkout
 
-Version:     2021.03.02
+Version:     2021.03.10
 
 Author:      Dintero
 Author URI:  mailto:integration@dintero.com
@@ -15,7 +15,7 @@ Domain Path: /languages
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'DINTERO_HP_VERSION', '2021.03.02' );
+define( 'DINTERO_HP_VERSION', '2021.03.10' );
 
 
 if ( ! defined( 'DHP_PLUGIN_FILE' ) ) {
