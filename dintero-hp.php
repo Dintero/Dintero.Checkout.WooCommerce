a8e877ff67740b04ce448208ca0d6bc3b45b8a6f
Anders Emil Salvesen <andersems@gmail.com>
2021-04-14 13:48:38 +0200

    Mark new version

--- dintero-hp.php
+++ dintero-hp.php
@@ -3,7 +3,7 @@
 Plugin Name: Dintero Checkout
 Description: Dintero Checkout - Express Checkout
 
-Version:     2021.03.31
+Version:     2021.04.14
 
 Author:      Dintero
 Author URI:  mailto:integration@dintero.com
@@ -15,7 +15,7 @@ Domain Path: /languages
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'DINTERO_HP_VERSION', '2021.03.31' );
+define( 'DINTERO_HP_VERSION', '2021.04.14' );
 
 
 if ( ! defined( 'DHP_PLUGIN_FILE' ) ) {
