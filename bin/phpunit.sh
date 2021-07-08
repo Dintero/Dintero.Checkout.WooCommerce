97254693fbf1073a014768fda2e981d9074e92a6
Anders Emil Salvesen <anders@dintero.com>
2021-07-08 13:42:23 +0200

    coverage (#66)
    * Adding coverage

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -4,4 +4,5 @@ if [[ $RUN_PHPCS == 1 || $SHOULD_DEPLOY == 1 ]]; then
 	exit
 fi
 
-if [ -f "phpunit.phar" ]; then php phpunit.phar -c phpunit.xml.dist; else ./vendor/bin/phpunit; fi;
+if [ -f "phpunit.phar" ]; then php phpunit.phar --coverage-clover clover.xml -c phpunit.xml.dist; else ./vendor/bin/phpunit --coverage-clover clover.xml; fi;
+pwd
