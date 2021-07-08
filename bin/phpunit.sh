e8b5d5a92bd1fcbb9ef66191b275a17272a91bfe
Anders Emil Salvesen <andersems@gmail.com>
2021-07-08 13:07:48 +0200

    Trying another path

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -4,4 +4,4 @@ if [[ $RUN_PHPCS == 1 || $SHOULD_DEPLOY == 1 ]]; then
 	exit
 fi
 
-if [ -f "phpunit.phar" ]; then php phpunit.phar --coverage-clover .clover.xml -c phpunit.xml.dist; else ./vendor/bin/phpunit --coverage-clover .clover.xml; fi;
+if [ -f "phpunit.phar" ]; then php phpunit.phar --coverage-clover ../clover.xml -c phpunit.xml.dist; else ./vendor/bin/phpunit --coverage-clover ../clover.xml; fi;
