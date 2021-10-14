d77ce793ff79a1ca3f6f527c16aeea9d6447568a
Anders Emil Salvesen <anders@dintero.com>
2021-10-14 08:57:54 +0200

    Release 2021.10.14 (#91)

--- README.txt
+++ README.txt
@@ -43,6 +43,10 @@ When you install Dintero Checkout, you need to head to the settings page to star
 
 == Changelog ==
 
+2021.10.14
+
+* Fix missing discount code on callback order
+
 2021.09.24
 
 * Fix wrong order note when payment was auto-captured
--- dintero-hp.php
+++ dintero-hp.php
@@ -2,7 +2,7 @@
 /**
 Plugin Name: Dintero Checkout
 Description: Dintero Checkout - Express Checkout
-Version:     2021.09.24
+Version:     2021.10.14
 Author:      Dintero
 Author URI:  mailto:integration@dintero.com
 Text Domain: dintero-hp
@@ -13,7 +13,7 @@ Domain Path: /languages
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'DINTERO_HP_VERSION', '2021.09.24' );
+define( 'DINTERO_HP_VERSION', '2021.10.14' );
 
 
 
