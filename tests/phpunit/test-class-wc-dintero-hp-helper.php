c143f82d785ebed4d1b9e6093bb0d8964626f869
Anders Emil Salvesen <andersems@gmail.com>
2021-10-28 13:02:46 +0200

    rename function in test

--- tests/phpunit/test-class-wc-dintero-hp-helper.php
+++ tests/phpunit/test-class-wc-dintero-hp-helper.php
@@ -9,37 +9,37 @@ class Dintero_HP_Helper_Test extends WP_UnitTestCase {
 	/**
 	 * @group helper
 	 */
-	public function test_get_metadata_null() {
+	public function test_convert_metadata_null() {
 		$helper = Dintero_HP_Helper::instance();
 		
-		$dintero_meta = $helper->get_metadata(null);
+		$dintero_meta = $helper->convert_to_dintero_metadata(null);
 		$this->assertEquals(null, $dintero_meta);
 	}
 	/**
 	 * @group helper
 	 */
-	public function test_get_metadata_empty() {
+	public function test_convert_metadata_empty() {
 		$helper = Dintero_HP_Helper::instance();
 		
-		$dintero_meta = $helper->get_metadata(array());
+		$dintero_meta = $helper->convert_to_dintero_metadata(array());
 		$this->assertEquals(null, $dintero_meta);
 	}
 	/**
 	 * @group helper
 	 */
-	public function test_get_metadata_straight_array() {
+	public function test_convert_metadata_straight_array() {
 		$helper = Dintero_HP_Helper::instance();
 		
-		$dintero_meta = $helper->get_metadata(array('a', 'b'));
+		$dintero_meta = $helper->convert_to_dintero_metadata(array('a', 'b'));
 		$this->assertEquals(null, $dintero_meta);
 	}
 	/**
 	 * @group helper
 	 */
-	public function test_get_metadata_object() {
+	public function test_convert_metadata_object() {
 		$helper = Dintero_HP_Helper::instance();
 		
-		$dintero_meta = $helper->get_metadata(array(
+		$dintero_meta = $helper->convert_to_dintero_metadata(array(
 			'a' => 'b'
 		));
 		$this->assertEquals(array('a' => 'b'), $dintero_meta);
