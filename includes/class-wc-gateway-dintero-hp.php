b1403f1d9e16bc7a5cd37cdfdc06ed3c58d099d5
Anders Emil Salvesen <anders@dintero.com>
2021-11-02 14:36:28 +0100

    Change on-hold explanation (#102)
    * Change on-hold explanation

--- includes/class-wc-gateway-dintero-hp.php
+++ includes/class-wc-gateway-dintero-hp.php
@@ -814,7 +814,7 @@ class WC_Gateway_Dintero_HP extends WC_Payment_Gateway
 					$note = __( 'Payment auto captured via Dintero. Transaction ID: ' ) . $dintero_order_transaction_id;
 					self::payment_complete( $order, $dintero_order_transaction_id, $note );
 				}elseif('ON_HOLD' === $transaction['status'] ){
-					$hold_reason = __( 'The payment is put on on-hold for manual review. The status of the payment will be updated when the manual review is finished. Transaction ID: ' ) . $dintero_order_transaction_id;
+					$hold_reason = __( 'The payment is put on on-hold for manual review by payment provider. The review will usually be finished within 5 minutes, and the status will be updated. Transaction ID: ' ) . $dintero_order_transaction_id;
 					self::on_hold_order( $order, $dintero_order_transaction_id, $hold_reason );
 				}elseif('FAILED' === $transaction['status'] ){
 					$hold_reason = __( 'The payment is not approved. Transaction ID: ' ) . $dintero_order_transaction_id;
@@ -1058,7 +1058,7 @@ class WC_Gateway_Dintero_HP extends WC_Payment_Gateway
 			 'dintero-hp' === $order->get_payment_method() ) {
 			$transaction_id = $order->get_transaction_id();
 			if (empty($transaction_id)) {
-				$order->add_order_note('Payment capture failed at Dintero because the order lacks transaction_id. Contact integration@dintero.com with order information.');
+				$order->add_order_note('Payment capture failed at Dintero because the order lacks transaction_id. Contact integration@dintero.com with order information. Changing status to on-hold.');
 				$order->set_status( 'on-hold' );
 				$order->save();
 				return false;
@@ -1074,7 +1074,7 @@ class WC_Gateway_Dintero_HP extends WC_Payment_Gateway
 			} else {
 				$order->add_order_note(__(
 					sprintf(
-						'Could not capture transaction: Merchant reference is wrong (%s, %s). Contact integration@dintero.com with order information.',
+						'Could not capture transaction: Merchant reference is wrong (%s, %s). Contact integration@dintero.com with order information. Changing status to on-hold.',
 						$merchant_reference, $merchant_reference_2,
 					)
 				));
@@ -1101,7 +1101,7 @@ class WC_Gateway_Dintero_HP extends WC_Payment_Gateway
 			}
 
 			if (isset($transaction['error'])) {
-				$order->add_order_note(__('Could not capture transaction: ' . $transaction['message']));
+				$order->add_order_note(__('Could not capture transaction: ' . $transaction['message'] . '. Changing status to on-hold.'));
 				$order->set_status( 'on-hold' );
 				$order->save();
 				$order->save_meta_data();
@@ -1117,7 +1117,7 @@ class WC_Gateway_Dintero_HP extends WC_Payment_Gateway
 			if ($this->extract('status', $transaction) !== 'AUTHORIZED') {
 				$order->add_order_note(__(
 					sprintf(
-						'Could not capture transaction: Transaction status is wrong (%s).',
+						'Could not capture transaction: Transaction status is wrong (%s). Changing status to on-hold.',
 						$this->extract('status', $transaction),
 					)
 				));
@@ -1152,7 +1152,7 @@ class WC_Gateway_Dintero_HP extends WC_Payment_Gateway
 				if (array_key_exists('error', $response_array) && isset($response_array['error']['message'])) {
 					$error_message = $response_array['error']['message'];
 				}
-				$note = sprintf('Payment capture failed at Dintero. Transaction ID: %s. Error message: %s', $transaction_id, $error_message);
+				$note = sprintf('Payment capture failed at Dintero. Transaction ID: %s. Error message: %s. Changing status to on-hold.', $transaction_id, $error_message);
 				$order->add_order_note( $note );
 				$order->set_status('on-hold');
 				$order->save();
@@ -1344,6 +1344,7 @@ class WC_Gateway_Dintero_HP extends WC_Payment_Gateway
 		if ( ! empty( $_GET['transaction_id'] ) ) {
 			$transaction_id = sanitize_text_field( wp_unslash( $_GET['transaction_id'] ));
 
+			// TODO: Calls with invalid id might come from here
 			$transaction = self::_adapter()->get_transaction($transaction_id);
 			if ( ! array_key_exists( 'merchant_reference', $transaction ) ) {
 				return false;
@@ -1367,7 +1368,7 @@ class WC_Gateway_Dintero_HP extends WC_Payment_Gateway
 						$note = __( 'Payment auto captured via Dintero. Transaction ID: ' ) . $transaction_id;
 						$this->payment_complete( $order, $transaction_id, $note );
 					}elseif('ON_HOLD' === $transaction['status'] ){
-						$hold_reason = __( 'The payment is put on on-hold for manual review. The status of the payment will be updated when the manual review is finished.. Transaction ID: ' ) . $transaction_id;
+						$hold_reason = __( 'The payment is put on on-hold for manual review by payment provider. The review will usually be finished within 5 minutes, and the status will be updated. Transaction ID: ' ) . $transaction_id;
 						$this->on_hold_order( $order, $transaction_id, $hold_reason );
 					}elseif('FAILED' === $transaction['status'] ){
 						$hold_reason = __( 'The payment is not approved. Transaction ID: ' ) . $transaction_id;
